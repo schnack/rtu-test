3c01355df960ab1df29c2a46b7ed28b56c39d065
Mikhail Stolyarov <schnack.desu@gmail.com>
2020-02-27 16:07:49 +0300

    add example slave, master

--- e2e/master-test.go
+++ e2e/master-test.go
@@ -1,5 +1,10 @@
 package e2e
 
+import (
+	"bufio"
+	"go.bug.st/serial"
+)
+
 type MasterTest struct {
 	Name       string   `yaml:"name"`
 	Skip       string   `yaml:"skip"`
@@ -20,4 +25,64 @@ type MasterTest struct {
 	WriteFormat []string            `yaml:"writeFormat"`
 	ReadFormat  []string            `yaml:"readFormat"`
 	ErrorFormat []string            `yaml:"errorFormat"`
+}
+
+func (mt *MasterTest) Run(port serial.Port) ReportMasterTest {
+	// TODO Требует реализации
+
+	scanner := bufio.NewScanner(port)
+	scanner.Split(frameAmsSplit)
+	scanner.Scan()
+
+	return ReportMasterTest{Name: mt.Name, Pass: true, Skip: mt.Skip}
+}
+
+// Сканер пакетов AMS
+func frameAmsSplit(data []byte, atEOF bool) (int, []byte, error) {
+	start := -1
+	stop := -1
+
+	if len(data) < minSizePackage {
+		return 0, nil, nil
+	}
+
+	for i := 0; i < len(data); i++ {
+		switch {
+		case i == 0:
+			// Пропускаем. Для определения начала пакета нужно 2 байта
+			continue
+		case data[i] == startBit && data[i-1] == startBit:
+			// Поиск стартовых битов
+			start = i - 1
+		case data[i] == stopBit && data[i-1] == stopBit:
+			// Поиск стоповых битов
+			stop = i + 1
+		}
+
+		// Удалось собрать пакет
+		if start != -1 && stop != -1 {
+			return stop, data[start:stop], nil
+		}
+	}
+
+	// Если отсуствуют данные для чтения
+	if atEOF {
+		return 0, data[:0], bufio.ErrFinalToken
+	}
+
+	switch {
+	case start == -1:
+		// Данные - мусор очищаем буфер
+		return len(data), data[:0], nil
+	case start != -1 && stop == -1:
+		// Нашли стартовый но не нашли стоповый бит. Если собираемый пакет превышает 255 байт то удаляем
+		if (len(data) - start) > 255 {
+			return len(data), data[:0], nil
+		} else {
+			return 0, nil, nil
+		}
+	}
+
+	// Запрашиваем еще данных удаляя мусор с сохранением одного бита
+	return len(data) - 1, nil, nil
 }
--- e2e/master.go
+++ e2e/master.go
@@ -1,5 +1,12 @@
 package e2e
 
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"go.bug.st/serial"
+	"strings"
+)
+
 type Master struct {
 	Port      string `yaml:"port"`
 	BoundRate int    `yaml:"boundRate"`
@@ -18,4 +25,75 @@ type Master struct {
 	ErrorFormat []string            `yaml:"errorFormat"`
 
 	Tests map[string][]*MasterTest `yaml:"tests"`
+}
+
+func (m *Master) getPort() (serial.Port, error) {
+	parity := serial.NoParity
+	switch m.Parity {
+	case "N":
+		parity = serial.NoParity
+	case "E":
+		parity = serial.EvenParity
+	case "O":
+		parity = serial.OddParity
+
+	}
+
+	stopBits := serial.TwoStopBits
+
+	switch m.StopBits {
+	case 1:
+		stopBits = serial.OneStopBit
+	case 15:
+		stopBits = serial.OnePointFiveStopBits
+	case 2:
+		stopBits = serial.TwoStopBits
+	}
+
+	return serial.Open(m.Port, &serial.Mode{
+		BaudRate: m.BoundRate,
+		DataBits: m.DataBits,
+		Parity:   parity,
+		StopBits: stopBits,
+	})
+}
+
+func (m *Master) Run(reports *ReportGroups) error {
+	// TODO Доработать этот функционал
+	port, err := m.getPort()
+	if err != nil {
+		return fmt.Errorf("port error %s", err)
+	}
+	defer port.Close()
+
+	filterGroup := ""
+	filterTest := ""
+	filter := strings.Split(m.Filter, ":")
+	if len(filter) > 1 {
+		filterGroup = filter[0]
+		filterTest = filter[1]
+	} else {
+		filterGroup = filter[0]
+	}
+
+	for group, tests := range m.Tests {
+		if filterGroup != "" && filterGroup != "all" && filterGroup != group {
+			continue
+		}
+		report := ReportGroup{Name: group}
+		logrus.Warnf(render(TestGROUP, report))
+		for _, test := range tests {
+			if filterTest != "" && filterTest != "all" && filterTest != test.Name {
+				continue
+			}
+			report.Tests = append(report.Tests, test.Run(port))
+			// При необходимости закрываем порт
+			if test.Disconnect {
+				port.Close()
+			}
+		}
+		reports.ReportGroup = append(reports.ReportGroup, report)
+	}
+
+	return nil
 }
